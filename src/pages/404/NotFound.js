abeab4b1484a155a8f404d48eaafa2246b8f00ca
Petter Johansson <petter.j@outlook.com>
2024-09-28 09:19:40 +0000

    ESLint: Added &apos;

--- src/pages/404/NotFound.js
+++ src/pages/404/NotFound.js
@@ -7,7 +7,7 @@ const NotFound = () => {
   return (
     <div className="container d-flex justify-content-center align-items-center vh-100">
       <div className="text-center">
-        <h1 className="mb-4">Oops! This page doesn't exist!</h1>
+        <h1 className="mb-4">Oops! This page doesn&apos;t exist!</h1>
         <Link to='/' className="btn btn-dark text-white d-flex align-items-center justify-content-center">
           <ArrowLeft size={24} className="me-2" />
           Go back to the home page
